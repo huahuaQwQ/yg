5ca3b4515207f833c22dbed4bba12622677e5369
plm1234568899 <62090626+plm1234568899@users.noreply.github.com>
2021-11-22 12:25:30 +0800

    runnable file

--- request/index.js
+++ request/index.js
@@ -20,7 +20,9 @@ export const request=(params)=>{
 
   // 定义公共的url
   //const baseUrl="https://api-hmugo-web.itheima.net/api/public/v1";
-  const baseUrl="api.weixin.qq.com/wxa/openapi";
+  //正式上线使用 https://test.api.nmxc.ltd/api
+  //const baseUrl="https://api-hmugo-web.itheima.net/api/public/v1";
+  const baseUrl="https://api-hmugo-web.itheima.net/api/public/v1";
   return new Promise((resolve,reject)=>{
     wx.request({
      ...params,
